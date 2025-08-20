ebecd36ee61a9b64f34d05631338193b9dd86cde
Tanuja <tanujathakur556@gmail.com>
2025-08-20 15:51:07 +0530

    Initial Commit

---
+++ blog/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, Depends
+
+from . import schemas, models
+
+from .database import Base, engine, Sessionlocal
+
+from sqlalchemy.orm import Session
+
+
+
+app=FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+
+
+def get_db():
+    
+    db = Sessionlocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
+
+
+
+@app.post('/blog')
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    
+    new_blog = models.Blog(title=request.title, body=request.body)
+    
+    db.add(new_blog)
+    
+    db.commit()
+    
+    db.refresh(new_blog)
+    
+    return new_blog
+
+
+@app.get('/blog')
+def all(db:Session = Depends(get_db)):
+    
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    
+    return blog
