360b456b219722e17efb3078864ffe5a4b561dbd
tanujathakur556-ui <tanujathakur556@gmail.com>
2025-08-22 11:22:57 +0530

    Update main.py

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 
 from . import schemas, models
 
@@ -26,18 +26,36 @@ def get_db():
 
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    
     new_blog = models.Blog(title=request.title, body=request.body)
-    
     db.add(new_blog)
-    
     db.commit()
-    
     db.refresh(new_blog)
-    
     return new_blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return "Done"
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request:schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    
+    blog.update(request)
+    db.commit()
+    return "Updated"
 
 
 @app.get('/blog')
@@ -47,9 +65,15 @@ def all(db:Session = Depends(get_db)):
     return blogs
 
 
-@app.get('/blog/{id}')
-def show(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}',status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
 
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    
+
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail= f"Blog with the {id} is not availabel")
+
+        #response.status_code = status.HTTP_404_NOT_FOUND
+        #return {'detalis': f"Blog with the {id} is not availabel"}
+
     return blog
