f766030305eb3698e72d321dac874d20ddb52cf2
tanujathakur556-ui <tanujathakur556@gmail.com>
2025-08-25 11:21:35 +0530

    Update

--- blog/schemas.py
+++ blog/schemas.py
@@ -4,4 +4,26 @@ class Blog(BaseModel):
     title:str
     body:str
 
-    
+class ShowBlog(BaseModel):
+    title:str
+    body:str
+    class Config:
+        orm_mode = True
+
+
+class User(BaseModel):
+    name:str
+    email: str
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+
+class ShowUser(BaseModel):
+    name:str
+    email: str
+
+    class Config:
+        orm_mode = True
