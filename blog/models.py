3dc8786e758e4c71f4dd7691a6684b90b459b5a0
tanujathakur556-ui <tanujathakur556@gmail.com>
2025-08-25 11:20:43 +0530

    Update

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 from .database import Base, engine, Sessionlocal
+
+from sqlalchemy.orm import relationship
 
 
 
@@ -12,4 +14,20 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
-    
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    creator = relationship("User", back_populates="blogs")
+
+
+class User(Base):
+    __tablename__="users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String)
+
+    email = Column(String)
+
+    password = Column(String)
+
+    blog = relationship("Blog", back_populates="creator")
